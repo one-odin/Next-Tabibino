a764bff4c630a21394ac1af9e817266909ba2215
one-odin <one.odin@yahoo.com>
2024-11-03 17:40:15 +0330

    Change button style in main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href="/add-user" className="bg-blue-700 text-white px-10 py-7">
+        <Link href="/add-user" className="bg-purple-700 text-white px-10 py-5 rounded-lg">
           افزودن کاربر جدید
         </Link>
       </main>
